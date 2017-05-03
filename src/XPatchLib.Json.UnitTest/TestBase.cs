df13d4e8555c060ed0db76954f79e6716c8a64f1
guqiangjs <guqiangjs@gmail.com>
2017-05-03 14:59:11 +0800

    Add UnitTest

---
+++ src/XPatchLib.Json.UnitTest/TestBase.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace XPatchLib.Json.UnitTest
+{
+    public abstract class TestBase
+    {
+        protected virtual void TraceResult(string testMethodName,string testResult)
+        {
+            Trace.WriteLine("**************");
+            Trace.WriteLine(testMethodName);
+            Trace.WriteLine("Result:");
+            Trace.WriteLine(testResult);
+            Trace.WriteLine("**************");
+        }
+
+        public void AssertEqual(string expected, string actual)
+        {
+            Assert.AreEqual(expected, actual);
+            TraceResult(new System.Diagnostics.StackTrace().GetFrame(1).GetMethod().Name, actual.ToString());
+        }
+    }
+}
